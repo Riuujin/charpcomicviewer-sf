ce051978032329dfbdeeee536670dc10962a76f3
Rutger Spruyt <raspruyt@raspruyt.nl>
2011-09-07 18:36:01 +0000

    - LoadArchives.cs is now ArchiveLoader.cs; class name change as well as function parameter change so it works with the new FileLoader class; - LoadReturnValue.cs is now FileLoader.cs; function has changed somewhat, not just a returnvalue anymore, for future loading of loose images. - FileNextPrevious.cs: removed CustomStackTrace and changed String's to string

---
+++ csharp-comicviewer/File/ArchiveLoader.cs
@@ -0,0 +1,188 @@
+﻿/*
+  Copyright 2011 Rutger Spruyt
+  
+  This file is part of C# Comicviewer.
+
+  csharp comicviewer is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  csharp comicviewer is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with csharp comicviewer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using SevenZip;
+using csharp_comicviewer.Comic;
+
+namespace csharp_comicviewer
+{
+    /// <summary>
+    /// Load archives using SevenZipSharp
+    /// </summary>
+    class ArchiveLoader
+    {
+        private ArrayList SupportedImageFormats = new ArrayList();
+        private InfoText_Form InfoText;
+
+        /// <summary>
+        /// Load the dll required
+        /// </summary>
+        public ArchiveLoader()
+        {
+            //Get the location of the 7z dll (location .EXE is in)
+            String executableName = Application.ExecutablePath;
+            FileInfo executableFileInfo = new FileInfo(executableName);
+            String executableDirectoryName = executableFileInfo.DirectoryName;
+
+            //load the  dll
+            SevenZipExtractor.SetLibraryPath(executableDirectoryName + "//7z.dll");
+
+            setSupported_image_formats();
+        }
+
+        /// <summary>
+        /// Set supported images types
+        /// </summary>
+        private void setSupported_image_formats()
+        {
+            SupportedImageFormats.Add(".jpg");
+            SupportedImageFormats.Add(".JPG");
+            SupportedImageFormats.Add(".bmp");
+            SupportedImageFormats.Add(".BMP");
+            SupportedImageFormats.Add(".png");
+            SupportedImageFormats.Add(".PNG");
+        }
+
+        /// <summary>
+        /// Creates a ComicBook from an array of archive paths
+        /// </summary>
+        /// <param name="Archives">Array of archive paths</param>
+        /// <returns>ComicBook</returns>
+        public bool Load(String[] Archives,out ComicBook Comicbook,out bool HasFile ,out string Error)
+        {
+            Error = null;
+            HasFile = false;
+        	Array.Sort(Archives);
+            Comicbook = new ComicBook();
+            String InfoTxt = "";
+            String CurrentFile;
+            List<byte[]> ImagesAsBytes = new List<byte[]>();
+            MemoryStream ms = new MemoryStream();
+            SevenZipExtractor Extractor;
+            Boolean NextFile = false;
+
+            foreach (String Archive in Archives)
+            {
+                if (!File.Exists(Archive))
+                {
+                    Error = "One or more archives where not found";
+                    return false;
+                }
+            }
+
+            try
+            {
+                for (int y = 0; y < Archives.Length; y++)
+                {
+                    //open archive
+                    CurrentFile = Archives[y];
+                    Extractor = new SevenZipExtractor(CurrentFile);
+                    String[] FileNames = Extractor.ArchiveFileNames.ToArray();
+                    Array.Sort(FileNames);
+
+                    //create ComicFiles for every single archive
+                    for (int i = 0; i < Extractor.FilesCount; i++)
+                    {
+                        for (int x = 0; x < SupportedImageFormats.Count; x++)
+                        {
+                            //if it is an image add it to arraylist
+                            if (FileNames[i].EndsWith(SupportedImageFormats[x].ToString()))
+                            {
+                                ms = new MemoryStream();
+                                Extractor.ExtractFile(FileNames[i], ms);
+                                ms.Position = 0;
+                                try
+                                {
+                                    ImagesAsBytes.Add(ms.ToArray());
+                                }
+                                catch (Exception)
+                                {
+                                    ms.Close();
+                                    Error = "One or more files are corrupted, and where skipped";
+                                }
+                                ms.Close();
+                                NextFile = true;
+                            }
+                            //if it is a txt file set it as InfoTxt
+                            else if (FileNames[i].EndsWith(".txt") || FileNames[i].EndsWith(".TXT"))
+                            {
+                                ms = new MemoryStream();
+                                Extractor.ExtractFile(FileNames[i], ms);
+                                ms.Position = 0;
+                                try
+                                {
+                                    StreamReader sr = new StreamReader(ms);
+                                    InfoTxt = sr.ReadToEnd();
+                                }
+                                catch (Exception)
+                                {
+                                    ms.Close();
+                                    Error = "One or more files are corrupted, and where skipped";
+                                }
+                                ms.Close();
+                                NextFile = true;
+                            }
+                            if (NextFile)
+                            {
+                                NextFile = false;
+                                x = SupportedImageFormats.Count;
+                            }
+                        }
+                    }
+                    //unlock files again
+                    Extractor.Dispose();
+
+                    //make a ComicFile, with either an InfoTxt or without
+                    if (ImagesAsBytes.Count > 0)
+                    {
+                        if (InfoTxt.Length > 0)
+                        {
+                            Comicbook.CreateComicFile(CurrentFile, ImagesAsBytes, InfoTxt);
+                            InfoText = new InfoText_Form(Archives[y], InfoTxt);
+                            InfoTxt = "";
+                        }
+                        else
+                            Comicbook.CreateComicFile(CurrentFile, ImagesAsBytes, null);
+                    }
+                    ImagesAsBytes.Clear();
+                }
+
+                if (Comicbook.HasFiles())
+                {
+                    HasFile = true;
+                }
+                else
+                    HasFile = false;
+
+                //return the ComicBook on succes
+                return true;
+            }
+            catch (Exception e)
+            {
+                //show error and return nothing
+                return false;
+            }
+        }
+    }
+}
---
+++ csharp-comicviewer/File/FileNextPrevious.cs
@@ -0,0 +1,304 @@
+﻿/*
+  Copyright 2011 Rutger Spruyt
+  
+  This file is part of C# Comicviewer.
+
+  csharp comicviewer is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  csharp comicviewer is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with csharp comicviewer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace csharp_comicviewer
+{
+    /// <summary>
+    /// Find and locate next and previous usable files(archives) on disk
+    /// </summary>
+    class FileNextPrevious
+    {
+        /// <summary>
+        /// Get next file in known file list
+        /// </summary>
+        /// <param name="CurrentFiles">Known file paths</param>
+        /// <returns>The next file path</returns>
+        public String[] GetNextFile(String[] CurrentFiles)
+        {
+            String[] Files = CurrentFiles;
+            Array.Sort(Files);
+            String file = Files.Last();
+
+            String nextfile = nextFile(file);
+            String dir = Directory.GetParent(file).ToString();
+            String lastdir = dir;
+            while (String.IsNullOrEmpty(nextfile))
+            {
+                lastdir = dir;
+                dir = nextDir(dir);
+                if (String.IsNullOrEmpty(dir))
+                {
+                    dir = nextDir(Directory.GetParent(lastdir).ToString());
+                }
+                if (String.IsNullOrEmpty(dir))
+                    break;
+
+
+                nextfile = firstOrLastFileInDir(dir, true);
+            }
+            String[] returnarray = new String[1];
+            returnarray[0] = nextfile;
+            return returnarray;
+        }
+
+        /// <summary>
+        /// Get previous file in known file list
+        /// </summary>
+        /// <param name="CurrentFiles">Known file path</param>
+        /// <returns>The previous file path</returns>
+        public String[] GetPreviousFile(String[] CurrentFiles)
+        {
+            String[] Files = CurrentFiles;
+            Array.Sort(Files);
+            String file = Files.Last();
+
+            String nextfile = previousFile(file);
+            String dir = Directory.GetParent(file).ToString();
+            String lastdir = dir;
+            while (String.IsNullOrEmpty(nextfile))
+            {
+                lastdir = dir;
+                dir = previousDir(dir);
+                if (String.IsNullOrEmpty(dir))
+                {
+                    dir = previousDir(Directory.GetParent(lastdir).ToString());
+                }
+                if (String.IsNullOrEmpty(dir))
+                    break;
+
+
+                nextfile = firstOrLastFileInDir(dir, false);
+            }
+            String[] returnarray = new String[1];
+            returnarray[0] = nextfile;
+            return returnarray;
+        }
+
+        /// <summary>
+        /// Get the next directory path to search in
+        /// </summary>
+        /// <param name="currentdir">The current directory path</param>
+        /// <returns>The next directory path</returns>
+        private String nextDir(String currentdir)
+        {
+            Boolean next = false;
+            try
+            {
+                String[] Dirs = Directory.GetDirectories(Directory.GetParent(currentdir).ToString());
+                Array.Sort(Dirs);
+                foreach (String dir in Dirs)
+                {
+                    if (next)
+                        return dir.ToString();
+
+                    if (currentdir == dir)
+                        next = true;
+                }
+            }
+            catch(Exception e)
+            {
+                return null;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Get the next usable file location on disk
+        /// </summary>
+        /// <param name="currentfile">Current file path</param>
+        /// <returns>The next file path</returns>
+        private String nextFile(String currentfile)
+        {
+            String dir = Directory.GetParent(currentfile).FullName;
+            String[] cbr = Directory.GetFiles(dir, "*.cbr");
+            String[] cbz = Directory.GetFiles(dir, "*.cbz");
+            String[] rar = Directory.GetFiles(dir, "*.rar");
+            String[] zip = Directory.GetFiles(dir, "*.zip");
+
+            List<String> filePaths = new List<String>();
+
+            foreach (String filename in cbr)
+            {
+                filePaths.Add(filename);
+            }
+            foreach (String filename in cbz)
+            {
+                filePaths.Add(filename);
+            }
+            foreach (String filename in rar)
+            {
+                filePaths.Add(filename);
+            }
+            foreach (String filename in zip)
+            {
+                filePaths.Add(filename);
+            }
+
+
+            filePaths.Sort();
+
+
+            Boolean next = false;
+            foreach (String filename in filePaths)
+            {
+                if (next)
+
+                    return filename;
+
+
+                if (currentfile == filename && filename != filePaths.Last())
+                    next = true;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Get the previous directory path to search in
+        /// </summary>
+        /// <param name="currentdir">The current directory path</param>
+        /// <returns>The previous directory path</returns>
+        private String previousDir(String currentdir)
+        {
+            try
+            {
+                String[] Dirs = Directory.GetDirectories(Directory.GetParent(currentdir).ToString());
+                Array.Sort(Dirs);
+                for(int i = 0; i < Dirs.Length;i++)
+                {
+                    if (currentdir == Dirs.First())
+                        return null;
+
+                    if (currentdir == Dirs[i])
+                        return Dirs[i - 1];
+                }
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Get the previous usable file location on disk
+        /// </summary>
+        /// <param name="currentfile">Current file path</param>
+        /// <returns>The previous file path</returns>
+        private String previousFile(String currentfile)
+        {
+            String dir = Directory.GetParent(currentfile).FullName;
+            String[] cbr = Directory.GetFiles(dir, "*.cbr");
+            String[] cbz = Directory.GetFiles(dir, "*.cbz");
+            String[] rar = Directory.GetFiles(dir, "*.rar");
+            String[] zip = Directory.GetFiles(dir, "*.zip");
+
+            List<String> filePaths = new List<String>();
+
+            foreach (String filename in cbr)
+            {
+                filePaths.Add(filename);
+            }
+            foreach (String filename in cbz)
+            {
+                filePaths.Add(filename);
+            }
+            foreach (String filename in rar)
+            {
+                filePaths.Add(filename);
+            }
+            foreach (String filename in zip)
+            {
+                filePaths.Add(filename);
+            }
+
+
+            filePaths.Sort();
+
+
+            for(int i= 0; i< filePaths.Count;i++)
+            {
+                if (currentfile == filePaths.First())
+                    return null;
+
+                if (currentfile == filePaths[i])
+                    return filePaths[i - 1];
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Get first or last file in a directory
+        /// </summary>
+        /// <param name="dir">Directory path</param>
+        /// <param name="First">Return first file?</param>
+        /// <returns>The file path</returns>
+        private String firstOrLastFileInDir(String dir,Boolean First)
+        {
+            try
+            {
+                String[] cbr = Directory.GetFiles(dir, "*.cbr");
+                String[] cbz = Directory.GetFiles(dir, "*.cbz");
+                String[] rar = Directory.GetFiles(dir, "*.rar");
+                String[] zip = Directory.GetFiles(dir, "*.zip");
+
+                List<String> filePaths = new List<String>();
+
+                foreach (String filename in cbr)
+                {
+                    filePaths.Add(filename);
+                }
+                foreach (String filename in cbz)
+                {
+                    filePaths.Add(filename);
+                }
+                foreach (String filename in rar)
+                {
+                    filePaths.Add(filename);
+                }
+                foreach (String filename in zip)
+                {
+                    filePaths.Add(filename);
+                }
+
+                if (filePaths.Count > 0)
+                {
+                    filePaths.Sort();
+
+                    if(First)
+                        return filePaths.First();
+                    else
+                        return filePaths.Last();
+                }
+                return null;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        
+
+    }
+}
